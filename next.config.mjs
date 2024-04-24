1a1a9a9e3a19f1a08685ea018ce349b9b56c5769
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-04-24 10:38:00 -0300

    Teste

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {reactStrictMode: true,
   images: {
     domains: ['store.storeimages.cdn-apple.com'],
   },
 }
-
-module.exports = nextConfig
+ 
+export default nextConfig
