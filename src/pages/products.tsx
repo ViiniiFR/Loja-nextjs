9e382cb29dbf81ebd4258c22b485b24c6e3f1b69
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-04-25 12:06:36 -0300

    Products

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,10 +1,15 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { ReactNode } from 'react' // Add missing import statement
 import { Container } from 'reactstrap'
 import Header from '../src/components/Header'
 import ProductsList from '../src/components/ProductsList'
-import { ProductType } from '../src/services/products'
+import { fetchProducts, ProductType } from '../src/services/products'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts()
+  return { props: { products } }
+}
 
 const Products: NextPage = (props: {
   children?: ReactNode
