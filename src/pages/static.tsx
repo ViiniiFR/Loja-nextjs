95cd543a87df06f5a2a168f5b6c0ea32a84098fe
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-04-25 15:37:16 -0300

    Products

--- src/pages/static.tsx
+++ src/pages/static.tsx
@@ -11,7 +11,7 @@ type ApiResponse = {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+    const staticData = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json.toString())
   
     return {
       props: {
