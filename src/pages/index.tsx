5b37dca5aae86f9b3431f02234559ca37f2c350f
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-04-26 11:23:22 -0300

    Add Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Header from '@/src/components/Header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
+import { Button, Container } from 'reactstrap'
 
 const Home: NextPage = () => {
   return (
@@ -13,14 +15,20 @@ const Home: NextPage = () => {
 
       <Header/>
 
-      <main>
-        <h1>
-          VINI STORE
-        </h1>
-
-        <p>
-          Mais Detalhes Sobre os Produtos abaixo
-        </p>
+      <main >
+        <Container className="py-5 text-center">
+          <h1 className="mt-5 display-1">
+            O melhor jeito de comprar o que você ama
+          </h1>
+          <p className="my-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae iusto voluptatem obcaecati omnis error architecto neque cum exercitationem fugiat. Vero illo autem eum nisi sapiente, odio optio accusamus cupiditate ad.
+          </p>
+          <Link href="/products">
+            <Button color="dark" className="px-4 pb-2">
+              Conheça nossos produtos!
+            </Button>
+          </Link>
+        </Container>
       </main>
     </>
   )
