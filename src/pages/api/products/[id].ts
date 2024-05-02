7c93f302352afe7117d67db67254d8350123f04e
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-05-02 12:41:09 -0300

    Adicionando Loja

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -5,5 +5,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query
 
   const product = products.find(p => p.id === Number(id))
-  res.status(200).json(product)
+  if (product) {
+    res.status(200).json(product)
+  } else {
+    res.status(404).json({ message: 'Product not found' })
+  }
 }
