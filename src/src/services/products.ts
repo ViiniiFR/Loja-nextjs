052e64bdab6b09c7964957047cbe13e5fb4041b6
Vinicius Santos <viniciussantos1janu@gmail.com>
2024-06-10 13:59:55 -0300

    Adicionando Loja

--- src/src/services/products.ts
+++ src/src/services/products.ts
@@ -8,11 +8,11 @@ export type ProductType = {
 }
 
 export const fetchProducts = async () => {
-  const products: ProductType[] = await fetch(`${process.env.NEXT_PUBLIC_APIURL}C:\Users\vinic\OneDrive\Documentos\Estudos Full Stack Javascript\MeuProjeto\black-nextjs\src\pages\api\products`).then(res => res.json())
+  const products: ProductType[] = await fetch(`${process.env.NEXT_PUBLIC_APIURL}src\pages\api\products`).then(res => res.json())
   return products
 }
 
 export const fetchProduct = async (id: string | number) => {
-  const product: ProductType = await fetch(`${process.env.NEXT_PUBLIC_APIURL}C:\Users\vinic\OneDrive\Documentos\Estudos Full Stack Javascript\MeuProjeto\black-nextjs\src\pages\api\products\[id].ts${id}`).then(res => res.json())
+  const product: ProductType = await fetch(`${process.env.NEXT_PUBLIC_APIURL}src\pages\api\products/${id}`).then(res => res.json())
   return product
 }
